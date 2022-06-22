91ed1cac3811e26427937000e2d1efe6840ec9ed
codeIsmaeel <debolaqasim@gmail.com>
2022-06-22 15:35:12 +0100

    CRUD blog

---
+++ blog/views.py
@@ -0,0 +1,55 @@
+
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+
+from django.views.generic.edit import CreateView
+
+from django.views.generic.detail import DetailView
+
+from django.views.generic.edit import UpdateView
+
+from django.views.generic.edit import DeleteView
+
+from .models import Post
+# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+
+# template_name = './blog/post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["__all__"]
+    success_url = reverse_lazy("blog:all")
+
+
+# template_name =
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# template_name = './blog/post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+# template_name =
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+# template_name = './blog/post_confirm_delete.html'
